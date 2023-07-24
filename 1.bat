1ba1fd94e6c6d84205293d77ca53761160190d0d
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2023-07-24 10:18:45 +0700

    1

---
+++ 1.bat
@@ -0,0 +1,3 @@
+SET "URL=https://www.softether-download.com/files/softether/v4.42-9798-rtm-2023.06.30-tree/Windows/SoftEther_VPN_Client/softether-vpnclient-v4.42-9798-rtm-2023.06.30-windows-x86_x64-intel.exe"
+start chrome -no-remote "%URL%"
+python python.py
---
+++ python.py
@@ -0,0 +1,15 @@
+import pyautogui as pg
+import time
+
+def click_image(image_path):
+    while True:
+        image_location = pg.locateOnScreen(image_path)
+        if image_location is not None:
+            image_center = pg.center(image_location)
+            pg.click(image_center)
+            break
+        else:
+            print("I am unable to see it")
+
+# First image
+click_image('./image/1keep.png')
