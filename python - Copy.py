2ed5ea829c1fa8c88ea685ca8353f7a876871ea5
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2023-07-24 14:25:01 +0700

    1

---
+++ python - Copy.py
@@ -0,0 +1,16 @@
+import pyautogui as pg
+import time
+
+def click_image(image_path):
+    while True:
+        image_location = pg.locateOnScreen(image_path)
+        if image_location is not None:
+            print("I can see it")
+            # image_center = pg.center(image_location)
+            # pg.click(image_center)
+            break
+        else:
+            print("I am unable to see it")
+
+# First image
+click_image('./image/28.png')
